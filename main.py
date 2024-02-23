d79158dba9bc240beca6ecb2437de2e3717e56d2
karamathasan <94758689+karamathasan@users.noreply.github.com>
2024-02-23 11:10:32 -0500

    more functions, added negative values to r

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 from matplotlib import  pyplot as plt
 import numpy as np
 
-t = 0.5#initial value of function, should not have an effect on the mapping, as long as its not 0
+t = 0.9 #initial value of function, should not have an effect on the mapping, as long as its not 0 and less than 1
+runLogisticMap = False #choose to run the logistic map or a new function
 
 def logisticMap(x,r):
     x = r * x * (1-x)
     return x
 
 def newMap(x,r):
-    x =  r/4 * np.sin(np.sin(x) * np.pi/np.sin(1))
+    # x =  (r/4) * np.sin(np.sin(x) * np.pi/np.sin(1))
+    # x = (27 * r /4) * x * x * (1-x)
+    # x = (r/4) * np.cos(np.pi * x)
+    x = (r/4) * np.sin(np.pi * x)
     return x
 
 def findConvergence(outputs):#returns the values cyclical values of a function after recursion has been applied
     convergedValues = []
-    initVal = 19 #arbitrary value, by 19 iterations the function should have been able to begin falling into a period 
+    initVal = 69 #arbitrary value, by this number of iterations the function should have been able to begin falling into a period 
     value = outputs[initVal]
     diff = outputs[initVal + 1]-value
     period = 0
     for i in range(len(outputs)):
-        if (i > initVal+1) and np.abs(outputs[i] - outputs[i-1] - diff) < 0.001 :
+        if (i > initVal+1) and np.abs(outputs[i] - outputs[i-1] - diff) < 0.0001 :
             period = i - (initVal + 1)
             break
     for i in range(period):
-        # print(outputs[initVal + i])
         convergedValues.append(outputs[initVal + i])
-    # convergedValues.sort()
     return convergedValues
-
-runLogisticMap = False
-
 
 def recurValues(iterationLimit,inputs,outputs,r):#recursively use function of choice for a given number of times
     for i in range(iterationLimit):
@@ -39,21 +38,14 @@ def recurValues(iterationLimit,inputs,outputs,r):#recursively use function of ch
             outputs.append(newMap(inputs[i+1],r))
 
 def getCyclicValues(iterationLimit,inputs,outputs,r):#recursively use function of choice and return the values that become cyclical
-    for i in range(iterationLimit):
-        inputs.append(outputs[i])
-        if (runLogisticMap):
-            outputs.append(logisticMap(inputs[i+1],r))
-        else:
-            outputs.append(newMap(inputs[i+1],r))
+    recurValues(iterationLimit,inputs,outputs,r)
     return findConvergence(outputs)
         
-def graphConverged(increment, x, y):#return coordinate pairs of converged/cyclic points of the function
-    # global x
-    # global y
-    # global r            
-    r = 0.0
-
-    # for j in range(int(4/increment)):
+negatives = True
+def Bifurcation(increment, x, y):#return coordinate pairs of the bifurcation diagram of the function   
+    r = 0
+    if (negatives):   
+        r = -4
     while r <= 4:
         inputs = [t]
         outputs = []
@@ -72,16 +64,21 @@ def graphConverged(increment, x, y):#return coordinate pairs of converged/cyclic
 
 x = []
 y = []
-coords = graphConverged(0.01,x,y)
+coords = Bifurcation(0.01,x,y)
 x = coords[0]
 y = coords[1]
-# print(len(x))
-# print(len(y))
 
-
-plt.scatter(x,y)
-plt.title("Logistic Map")
+if (runLogisticMap):
+    plt.suptitle("rx(1-x)")
+plt.scatter(x,y,s=0.5)
+plt.title("Bifurcation Diagram")
 plt.autoscale(False)
 plt.xlim(0,4)
+if (negatives):
+    plt.xlim(-4,4)
+plt.xlabel("r")
 plt.ylim(0,1)
+plt.ylabel("converged value")
 plt.show()
+
+# print(getCyclicValues())
